bd7241a71f099ff73095dc9e892d63c3c18f2b26
Ivan <ivan@softwarecriollo.com>
2015-12-03 16:32:41 -0500

    adroll

--- app.rb
+++ app.rb
@@ -14,6 +14,12 @@ class App < Sinatra::Base
     def partial(page, options={})
       haml page, options.merge!(:layout => false)
     end
+
+    def erb_partial(page, options={})
+      erb page, options.merge!(:layout => false)
+    end
+
+
     
     def partial(page, options={})
       slim page, options.merge!(:layout => false)
